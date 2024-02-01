6042294e0059116284332e682e4fa5a275bea3bc
MarcAlvarezz <157707905+MarcAlvarezz@users.noreply.github.com>
2024-02-01 14:24:20 +0100

    4

--- 004 Estructures de Control de Selecció if & switch/EX2/EX2.ino
+++ 004 Estructures de Control de Selecció if & switch/EX2/EX2.ino
@@ -1,38 +1,29 @@
 /**********************************************************************************
 **                                                                               **
-**                   Programa de Qualificació Energètica                           **
+**                          Control Structures                                   **
+**                                                                               **
 **                                                                               **
 **********************************************************************************/
 
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-float kgCO2_per_m2 = 20.0;  // Exemple de valor, pots canviar-ho amb la teva mesura
+int tempAigua = 101;
 
 //********** Setup ****************************************************************
-void setup() {
-  Serial.begin(9600);  // Inicia la comunicació sèrie a 9600 bps
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  if (tempAigua > 100)
+  {
+    Serial.print("Aigua supera els 100C, esta bullint!");
+  } 
 }
 
 //********** Loop *****************************************************************
-void loop() {
-  // Determina la qualificació energètica basada en els intervals especificats
-  if (kgCO2_per_m2 < 3.5) {
-    Serial.println("Qualificació Energètica: A");
-  } else if (kgCO2_per_m2 < 6.5) {
-    Serial.println("Qualificació Energètica: B");
-  } else if (kgCO2_per_m2 < 11.1) {
-    Serial.println("Qualificació Energètica: C");
-  } else if (kgCO2_per_m2 < 17.7) {
-    Serial.println("Qualificació Energètica: D");
-  } else if (kgCO2_per_m2 < 38.2) {
-    Serial.println("Qualificació Energètica: E");
-  } else if (kgCO2_per_m2 < 43.2) {
-    Serial.println("Qualificació Energètica: F");
-  } else {
-    Serial.println("Qualificació Energètica: G");
-  }
-
-  // No fem res més en el loop ja que només volem mostrar la qualificació una vegada
-  while (1);
+void loop()   // we need this to be here even though its empty
+{
 }
+
+//********** Funcions *************************************************************
