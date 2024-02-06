ed2bc1aea16bf383d5e1549ec900af0f73428954
MarcAlvarezz <157707905+MarcAlvarezz@users.noreply.github.com>
2024-02-06 08:15:51 +0100

    ex5 i 6

---
+++ 005 Estructures de Control de Repetició for & while/Ex5/Ex5.ino
@@ -0,0 +1,45 @@
+/**********************************************************************************
+**                                                                               **
+**                          Control Structures                                   **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+int inicioTabla = 2;
+int finTabla = 10;
+
+//********** Setup ****************************************************************
+void setup()      // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  for (int taula = inicioTabla; taula <= finTabla; taula++)
+  {
+    Serial.print("Taula de multiplicar del ");
+    Serial.println(taula);
+
+    for (int i = 0; i <= 10; i++)
+    {
+      delay(100);
+      Serial.print(taula);
+      Serial.print(" x ");
+      Serial.print(i);
+      Serial.print(" = ");
+      Serial.println(taula * i);
+    }
+    Serial.println(); // Añadir una línea en blanco entre tablas para mayor claridad
+  }
+
+  // Salir después de imprimir las tablas
+  while (true) {
+    // Esto evitará que el programa entre en el bucle de loop repetidamente
+  }
+}
+
+//********** Loop *****************************************************************
+void loop()   // we need this to be here even though its empty
+{
+}
